bbece4e4d61d6e14ed685f17d462182d0df78e29
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-08-16 16:54:11 -0400

    Se creó modelo Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-products_table
-ActiveRecord::Schema.define(version: 2021_08_16_185953) do
-
-
-
+ActiveRecord::Schema.define(version: 2021_08_16_202810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.boolean "active"
+    t.integer "total_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -53,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_08_16_185953) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
 end
