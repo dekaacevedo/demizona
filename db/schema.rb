a1afbd87b3dd57dcd6bf965edcc2dc8f40c4d0bc
Jorge Molina <jorgemolinan@gmail.com>
2021-08-16 17:12:03 -0400

    category model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-products_table
 ActiveRecord::Schema.define(version: 2021_08_16_185953) do
-
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
