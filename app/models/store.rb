f908154a45cbb63cb31edbc430da7b85cd509327
marco fernandez <maaferna@gmail.com>
2021-08-17 17:34:28 -0400

    agregadas validaciones

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,11 @@
 class Store < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
+  has_many :products, dependent: :destroy
+
+  validates :name, presence: true,  length: { in: 2..30 }
+  validates :address, presence: true
+  validates :city, presence: true,  length: { in: 2..30 }
+  validates :email, presence: true, format: { with: /^[\w|-]+@[\w|-]+\.\w+$/ }
+  validates :phone, presence: true, length: { minimum: 9 }, numericality: { only_integer: true }
 end
