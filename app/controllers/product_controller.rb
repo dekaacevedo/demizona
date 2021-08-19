16f65f77d80e298aba64f4372d4b0ff407456a09
Jorge Molina <jorgemolinan@gmail.com>
2021-08-19 15:32:53 -0400

    Creado controlador de Productos y vistas vacias

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,41 @@
+class ProductController < ApplicationController
+
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+  def edit    
+  end
+
+  def update
+    @product.update(product_params)
+  end
+
+  def destroy
+    @product.destroy
+  end
+
+private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def products_params
+    params.require(:params).permit(:name, :description, :price, :active)
+  end
+end
