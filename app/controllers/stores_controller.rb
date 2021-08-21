a8475c3d716ea0a313669bb0db9a52449a647a51
pablo martinez <pablomartinezsepulveda@hotmail.com>
2021-08-21 04:14:01 -0400

    Se creó mapa con buscador y posibilidad de redireccionar a show

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,7 +2,31 @@ class StoresController < ApplicationController
 
   before_action :set_store, only: [:show]
 
+  def index
+
+    @stores = Store.geocoded # trae stores´s que tengan latitude y longitude obtenidas con la gema
+    @markers = @stores.geocoded.map do |store|
+      {
+        lat: store.latitude,
+        lng: store.longitude,
+        info_window: render_to_string(partial: "info_index",
+        locals: { store: store }),
+        # image_url: helpers.asset_url("marker.png")
+      }
+    end
+  end
+
   def show
+
+    @markers = [
+      {
+        id: @store.id,
+        lat: @store.latitude,
+        lng: @store.longitude,
+        info_window: render_to_string(partial: "info_window",
+        locals: { store: @store })
+      }
+    ]
   end
 
 private
