88869cfe2a693f651534bc7bc892f0dc106a65cb
Andrea Acevedo <andreaacevedo6@gmail.com>
2021-08-24 15:00:41 -0400

    cambios en application controller, before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # agregar solo donde sea necesario (carrito)
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   include Pundit
 
    # Pundit: white-list approach.
