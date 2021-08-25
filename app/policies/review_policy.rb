9fc68a95ffdeae8a6891385944c76f2b1b33e583
Andrea Acevedo <andreaacevedo6@gmail.com>
2021-08-25 18:03:55 -0400

    correcion de errores

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,14 @@
+class ReviewPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def create?
+    true
+  end
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
