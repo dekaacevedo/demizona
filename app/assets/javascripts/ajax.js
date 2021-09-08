a6342a5cfc71d078566fb32f766d4a7ad90fb066
Pablo Martínez <79098599+pabloems@users.noreply.github.com>
2021-09-08 16:29:43 -0300

    Delete ajax.js

--- app/assets/javascripts/ajax.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	let button = document.querySelector('show-reviews');
-
-	button.addEventListener('click', function(event) {
-	  console.log(event.type + ' event detected!');
-	});
-});
