8887bf7f07bb5df163bc3cf3efea80e11d97c26b
dekaacevedo <83292633+dekaacevedo@users.noreply.github.com>
2021-09-09 15:40:35 -0300

    estilos en show de category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :set_category , only: :show
   def show
+    @pagy,products = pagy(policy_scope(Product),items: 9)
     @products = @category.products
     @cart_item = current_cart.cart_items.new
   end
